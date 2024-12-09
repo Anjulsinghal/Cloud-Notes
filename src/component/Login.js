e27992b12daa695c0a7b37153ac0d643425c5d96
Anjulsinghal <95030018+Anjulsinghal@users.noreply.github.com>
2024-12-09 22:04:53 +0530

    Update Login.js
    add real backend server

--- src/component/Login.js
+++ src/component/Login.js
@@ -6,7 +6,7 @@ const Login = (props) => {
   let navigate = useNavigate();
   const handelSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://cloud-notes-2.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
